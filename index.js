6e6dd38c8bdd8093046b88c874684e24dfd25acb
adarvin14 <amdarvin14@gmail.com>
2021-02-18 13:37:11 -0500

    commit tree 32aa21bc565b85f0bcd51208baf999a8a010b30d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded")
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    const a = document.querySelector("#text")
+    a.innerHTML = "This is really cool!"
+  })
